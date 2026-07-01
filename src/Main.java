99b0e062d076170f994da576dcff32f4a8d043b4
Nguyen Nhat Cuong <nguyennhatcuong2004@icloud.com>
2026-07-01 07:16:59 +0700

    first commit

---
+++ src/Main.java
@@ -0,0 +1,46 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    //Thuật toán sắp xếp bọt
+    public static void bubbleSort(int[] arr){
+        boolean neednNextPass=true;
+        for(int i=1;i<arr.length && neednNextPass;i++){
+            neednNextPass=false;
+            for (int j=0;j<arr.length-i;j++){
+                if (arr[j+1]>arr[j]){
+                    //Thuật toán hoán đổi
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                    neednNextPass=true;
+                }
+            }
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Nhập số lượng phần tử của mảng: ");
+        int arr_lenght= sc.nextInt();
+        int[] arr= new int[arr_lenght];
+        System.out.println("Nhập các phần tử của mảng:");
+        for (int i=0; i<arr.length;i++){
+            System.out.print("Nhập phần tử thứ "+ (i+1)+": ");
+            if (sc.hasNextInt()){
+                arr[i]=sc.nextInt();
+            }
+            else {
+                System.err.println("Lỗi vui lòng nhập lại");
+                sc.next();
+                i--;
+            }
+        }
+        bubbleSort(arr);
+        System.out.println("Mảng sau khi phàn tử sắp xếp giảm dần:");
+        System.out.println(Arrays.toString(arr));
+        sc.close();
+
+
+    }
+}
+
